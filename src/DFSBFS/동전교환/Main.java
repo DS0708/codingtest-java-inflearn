1ea5c71cc77852f83db48f0593dc103278f79450
DS0708 <jonum12312@gmail.com>
2025-02-06 10:32:52 +0900

    Greedy + DFS (R)

--- src/DFSBFS/동전교환/Main.java
+++ src/DFSBFS/동전교환/Main.java
@@ -1,4 +1,27 @@
 package DFSBFS.동전교환;
 
+import java.util.*;
+
 public class Main {
+    static int n,change;
+    static Integer[] coins;
+    static int answer = Integer.MAX_VALUE;
+    public static void DFS(int num, int money){
+        if(num >= answer) return;
+        if(money > change) return;
+        else if (money==change) answer = Math.min(num,answer);
+        else{
+            for(int i=0; i<coins.length; i++) DFS(num+1,money+coins[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        coins = new Integer[n];
+        for(int i = 0; i < n; i++) coins[i] = scanner.nextInt();
+        Arrays.sort(coins,Collections.reverseOrder());
+        change = scanner.nextInt();
+        DFS(0,0);
+        System.out.println(answer);
+    }
 }
